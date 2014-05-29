e8ec222189e55e3737c91d5294ed911be252b865
Matt Godbolt <mgodbolt@drwholdings.com>
2014-05-28 20:27:58 -0500

    Loads more for part 3

--- pygen/asm_lexer.py
+++ pygen/asm_lexer.py
@@ -10,17 +10,18 @@ class AsmLexer(RegexLexer):
         'root': [
             (r'\.[a-zA-Z_]+', Name.Label),
             (r';.*\n', Comment),
-            (r'&[0-9a-fA-F]+', Literal.Number.Hex),
+            (r'[&$][0-9a-fA-F]+', Literal.Number.Hex),
             (r'-?[0-9]+', Literal.Number.Integer),
             (r'(\[|\])', Punctuation),
-            (r'[-#,{}!:]', Punctuation),
-            (r'(R[0-9]+|PC)', Name.Variable),
+            (r'[-,{}!:()#=?.]', Punctuation),
+            (r'(R[0-9]+|PC|X|Y)', Name.Variable),
             (r'"[^"]+"', Literal.String),
             (r'(STR|LDR|ADD|SUB|SWI|AND|ORR|CMP|BIC|MOVS?|LDM|STM|B)'
                 r'(NE|EQ|GT|GE|LT|LE|VC|VS)?'
                 r'(FD)?'
                 , Token.Keyword),
-            (r'[a-zA-Z_]+',Name.Label),
+            (r'(LDA|LDX|LDY|STA|STX|STY|CMP|CPX|CPY|INC|INX|INY|RTS|ADC|CLC|TAY|TAX|ROL|ROR)', Token.Keyword),
+            (r'[a-zA-Z_]+', Name.Label),
             (r' +', Text),
         ]
     }
