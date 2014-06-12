976c10ca57f5a258eb6edddedc45e110a69e1155
Matt Godbolt <mgodbolt@drwholdings.com>
2014-06-12 13:55:57 -0500

    New stuff

--- pygen/asm_lexer.py
+++ pygen/asm_lexer.py
@@ -8,20 +8,20 @@ class AsmLexer(RegexLexer):
 
     tokens = {
         'root': [
-            (r'\.[a-zA-Z_]+', Name.Label),
+            (r'\.[a-zA-Z_][a-zA-Z_0-9]*', Name.Label),
             (r';.*\n', Comment),
             (r'[&$][0-9a-fA-F]+', Literal.Number.Hex),
             (r'-?[0-9]+', Literal.Number.Integer),
             (r'(\[|\])', Punctuation),
-            (r'[-,{}!:()#=?.]', Punctuation),
-            (r'(R[0-9]+|PC|X|Y)', Name.Variable),
+            (r'[-,{}!:()#=?.+%]', Punctuation),
             (r'"[^"]+"', Literal.String),
+            (r'(LD[AXY]|ST[AXY]|CMP|CP[XY]|IN[CXY]|RT[SI]|ADC|SBC|SEC|CLC|T[AXY][AXY]|RO[LR]|JSR|JMP|DIV|MOD|ASL|P[HL][AXY]|BIT|DE[CXY]|ORA|EOR)', Token.Keyword),
             (r'(STR|LDR|ADD|SUB|SWI|AND|ORR|CMP|BIC|MOVS?|LDM|STM|B)'
-                r'(NE|EQ|GT|GE|LT|LE|VC|VS)?'
+                r'(NE|EQ|GT|GE|LT|LE|VC|VS|PL|MI|CC|CS)?'
                 r'(FD)?'
                 , Token.Keyword),
-            (r'(LDA|LDX|LDY|STA|STX|STY|CMP|CPX|CPY|INC|INX|INY|RTS|ADC|CLC|TAY|TAX|ROL|ROR)', Token.Keyword),
-            (r'[a-zA-Z_]+', Name.Label),
+            (r'(R[0-9]+|PC|X|Y|A|P)', Name.Variable),
+            (r'[a-zA-Z_][a-zA-Z_0-9]*', Name.Label),
             (r' +', Text),
         ]
     }
